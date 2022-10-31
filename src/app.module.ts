7f5028de7d7007d74e1e6127f8b3d55c8ecb1061
endy <7ofpentacles@gmail.com>
2022-10-31 21:31:03 +0700

    add typeorm config with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// !
+import { BookModule } from './book/book.module';
+import { CommonModule } from './common/common.module';
+
+const isEnable = ['dev', 'dev-local'].includes(process.env.NODE_ENV);
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: isEnable,
+    }),
+    BookModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
